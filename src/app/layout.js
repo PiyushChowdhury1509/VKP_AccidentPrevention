e4f26b0f01bd86562a08d933e8608a43f4aaffd3
Piyush Chowdhury <piyushchowdhury1509@gmail.com>
2024-08-20 00:02:25 +0530

    added back navbar and improved privacy policy ui

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <SessionProvider>
+          <Navbar/>
           {children}
         </SessionProvider>
       </body>
